3e11caca464b53b2d6aa4b9ee8367b1f1be7ee47
Michael Jurkoic <mjjurkoic@gmail.com>
2019-10-29 09:37:04 -0500

    Finished Homework 5

--- DatesPlayground.playground/Contents.swift
+++ DatesPlayground.playground/Contents.swift
@@ -1,16 +1,21 @@
 import Foundation
 
-// Start with a date, like so:
+// Get today's date
+let todaysDate: Date = Date()
 
-let date = Date()
+// Create a new date for Halloween
+let calendar = Calendar(identifier: .gregorian)
+let halloweenDateComponents = DateComponents(year: 2019, month: 10, day: 31)
+let halloweenDate = calendar.date(from: halloweenDateComponents)
 
-// Create a date using today's date and a DateComponents structure
+// Compare today's date to Halloween and say whether they're the same
+if (calendar.isDate(todaysDate, inSameDayAs:halloweenDate!)) {
+    print("Today is Halloween!")
+} else {
+    print("It's not spooky time yet.")
+}
 
-// Compare the two dates to see if they occur in the same Calendar day
-
-// Use a date formatter
-
+// Print today's date nicely formatted
 let formatter = DateFormatter()
-
-// use the formatter to print the day in a friendly format.
-// hint, use NSDateFormatter.com to get an appropriate printing format string.
+formatter.dateFormat = "EEEE, d MMMM yyyy"
+print(formatter.string(from: todaysDate))
